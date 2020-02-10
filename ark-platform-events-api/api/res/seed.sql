b28ae7c09ffd545b06369b22b4161dbd547ba758
okarim4259 <okarim4259@gmail.com>
2020-02-10 02:17:28 -0500

    Added Core ML models

--- ark-platform-events-api/api/res/seed.sql
+++ ark-platform-events-api/api/res/seed.sql
@@ -1,18 +1,18 @@
 select * from activity_ref;
 
-INSERT INTO activity_ref (id, activity_type) VALUES (0, 'Starting a business');
-INSERT INTO activity_ref (id, activity_type) VALUES (1, 'Start Up Work Space');
-INSERT INTO activity_ref (id, activity_type) VALUES (2, 'Corporate');
-INSERT INTO activity_ref (id, activity_type) VALUES (3, 'Networking Event');
-INSERT INTO activity_ref (id, activity_type) VALUES (4, 'Work Shop');
-INSERT INTO activity_ref (id, activity_type) VALUES (5, 'Party');
-INSERT INTO activity_ref (id, activity_type) VALUES (6, 'Performances');
-INSERT INTO activity_ref (id, activity_type) VALUES (7, 'Wedding');
-INSERT INTO activity_ref (id, activity_type) VALUES (8, 'Photo Shoot');
-INSERT INTO activity_ref (id, activity_type) VALUES (9, 'Pop Up');
-INSERT INTO activity_ref (id, activity_type) VALUES (10, 'Dinner');
-INSERT INTO activity_ref (id, activity_type) VALUES (11, 'Trip with friends');
-INSERT INTO activity_ref (id, activity_type) VALUES (12, 'Art Exhibit');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (0, 'Starting a business');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (1, 'Start Up Work Space');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (2, 'Corporate');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (3, 'Networking Event');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (4, 'Work Shop');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (5, 'Party');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (6, 'Performances');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (7, 'Wedding');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (8, 'Photo Shoot');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (9, 'Pop Up');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (10, 'Dinner');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (11, 'Trip with friends');
+-- INSERT INTO activity_ref (id, activity_type) VALUES (12, 'Art Exhibit');
 
 
 
