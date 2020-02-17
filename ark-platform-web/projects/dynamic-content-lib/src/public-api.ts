6eed8c42f9e25fef67b7216bc4eae735c0c40e23
okarim4259 <okarim4259@gmail.com>
2020-02-17 01:26:15 -0500

    Remove project

--- ark-platform-web/projects/dynamic-content-lib/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of dynamic-content-lib
- */
-
-export * from './lib/dynamic-content-lib.service';
-export * from './lib/dynamic-content-lib.component';
-export * from './lib/dynamic-content-lib.module';
